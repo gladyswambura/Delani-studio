26bf5d2fd77102b30ae511e2bfc12c6254d9b0dd
gladyswambura <gladyswahito7@gmail.com>
2022-03-12 16:23:44 +0300

    OPtimizes the functionality of .show and .toggle attributes of jquery

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,7 @@
 // user interface
 $(document).ready(function() {
+
+    // design
     $("#design-show").click(function() {
         $("#design-show").hide();
         $("#design-hide").toggle();
@@ -9,6 +11,17 @@ $("#design-hide").click(function() {
     $("#design-show").toggle();
 });
 
+// development
+$("#development-show").click(function () {
+    $("#development-show").hide();
+    $("#development-hide").toggle();
+});
+$("#development-hide").click(function () {
+    $("#development-hide").hide();
+    $("#development-show").toggle();
+});
+
+// product-management
 
 
 
