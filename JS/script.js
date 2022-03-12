b929e4b25f5465c5fd1f75e85dbccd1a4a1f716d
gladyswambura <gladyswahito7@gmail.com>
2022-03-12 11:53:22 +0300

    FIX: fixed the flex-row bug

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// user interface
+
+$(document).ready(function () {
+$("#design-show").click(function () {
+$("#design-show").hide();
+});
+
+$("#design-hide").toggle();
+});
+
+$("#design-hide").click(function () {
+$("#design-hide").hide();
+$("#design-show").toggle();
+});
+});
