9497814ac18d7d119b91988ce8378db025293a4c
gladyswambura <gladyswahito7@gmail.com>
2022-03-13 14:08:37 +0300

    Fix: fixes the event.preventDefault()

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,54 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // design
-    $("#design-show").click(function() {
+    $("#design-show").click(function () {
         $("#design-show").hide();
         $("#design-hide").toggle();
     });
-$("#design-hide").click(function() {
-    $("#design-hide").hide();
-    $("#design-show").toggle();
-});
+    $("#design-hide").click(function () {
+        $("#design-hide").hide();
+        $("#design-show").toggle();
+    });
 
-// development
-$("#development-show").click(function () {
-    $("#development-show").hide();
-    $("#development-hide").toggle();
-});
-$("#development-hide").click(function () {
-    $("#development-hide").hide();
-    $("#development-show").toggle();
-});
+    // development
+    $("#development-show").click(function () {
+        $("#development-show").hide();
+        $("#development-hide").toggle();
+    });
+    $("#development-hide").click(function () {
+        $("#development-hide").hide();
+        $("#development-show").toggle();
+    });
 
-// product-management
-$("#Product-management-show").click(function () {
-    $("#Product-management-show").hide();
-    $("#Product-management-hide").toggle();
-});
-$("#Product-management-hide").click(function () {
-    $("#Product-management-hide").hide();
-    $("#Product-management-show").toggle();
-});
+    // product-management
+    $("#Product-management-show").click(function () {
+        $("#Product-management-show").hide();
+        $("#Product-management-hide").toggle();
+    });
+    $("#Product-management-hide").click(function () {
+        $("#Product-management-hide").hide();
+        $("#Product-management-show").toggle();
+    });
 
 });
 
 // form
 $(document).ready(function () {
-    $("form#contact-form").submit(function () {
+    $("form#contactform").submit(function () {
         var personName = $("#person-name").val();
-            var userMail = $("#person-email").val();
-                var userMessage = $("#user-message").val();
+        var userMail = $("#person-email").val();
+        var userMessage = $("#user-message").val();
+        if (personName != '') {
+            alert(personName + " , we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Check your entries");
+        }
+    })
+})
 
+let btn = document.getElementById('submit');
+btn.addEventListener('click', (event) => {
+    event.preventDefault()
 
-
+    // submitQuery();
+})
